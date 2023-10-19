3130e9fdb311e4d21561f50a992f829bce530028
SokuRitszZ <1062708758@qq.com>
2023-10-20 02:11:18 +0800

    chore: 处理子路由无法访问

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,18 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import uno from 'unocss/vite';
 import { defineConfig } from 'vite';
+import { viteStaticCopy as copy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     uno(),
+    copy({
+      targets: [
+        { src: './_redirects', dest: '' },
+      ],
+    }),
   ],
   resolve: {
     alias: {
