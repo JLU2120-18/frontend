820bf6ad6aeb3a3c3bb8853527499efcfa1308a8
SokuRitszZ <1062708758@qq.com>
2023-10-20 01:41:54 +0800

    chore: 接通获取用户接口

--- src/routes/employee/request.ts
+++ src/routes/employee/request.ts
@@ -1,5 +1,4 @@
-import { sleep } from '@/utils';
-import Mock from 'mockjs';
+import { api } from '@/requests';
 
 interface GetPurchaseOrderRequest {
   jwt: string;
@@ -10,22 +9,8 @@ interface GetPurchaseOrderRequest {
 
 
 export const GetEmployeeReq = async (params: GetPurchaseOrderRequest) => {
-  await sleep(2000);
-  return Mock.mock({
-    total: 100,
-    [`data|${params.pageSize}`]: [{
-      id: '@guid',
-      username: '@name',
-      address: '@address',
-      phone: '@phone',
-      socsecId: '@guid',
-      taxRate: '@float(0.0,0.3)',
-      otherCast: '@integer(100,500)',
-      type: Mock.Random.pick(['salary', 'commission', 'wage']),
-      hourWage: '@integer(50,60)',
-      salary: '@integer(4000,8000)',
-      commissionRate: '@float(0.1,0.4)',
-      durationLimit: '@integer(40,50)',
-    }],
+  const result = await api.get('/employee/gets', {
+    params,
   });
+  return result.data;
 };
